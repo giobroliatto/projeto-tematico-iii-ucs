d5e28f3fe4446ecd3243c38a00c6160eea98b222
gio broliatto <84979354+giobroliatto@users.noreply.github.com>
2024-04-16 20:17:48 -0300

    feat: crud operations to ecopontos

--- src/routes/ecoponto-routes.js
+++ src/routes/ecoponto-routes.js
@@ -4,5 +4,9 @@ import EcopontoController from "../controllers/ecoponto-controller.js";
 const routes = express.Router();
 
 routes.get("/ecopontos", EcopontoController.getEcopontos);
+routes.get("/ecopontos/:id", EcopontoController.getEcopontoById);
+routes.post("/ecopontos", EcopontoController.createEcoponto);
+routes.put("/ecopontos/:id", EcopontoController.updateEcoponto);
+routes.delete("/ecopontos/:id", EcopontoController.removeEcoponto);
 
 export default routes;

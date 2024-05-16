c09fa763121c03ab5077a97f28c9c2ecd579d1c5
gio broliatto <84979354+giobroliatto@users.noreply.github.com>
2024-05-15 22:50:17 -0300

    feat: tooltip added

--- frontend/src/components/IconsHeader/index.js
+++ frontend/src/components/IconsHeader/index.js
@@ -1,4 +1,4 @@
-// IconsHeader.js
+import { Tooltip } from 'react-tooltip';
 import s from './style.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
@@ -10,15 +10,22 @@ function IconsHeader({ onOptionClick }) {
         onOptionClick(option);
     };
 
-    return(
-        <ul className={s.icons}>
-            {icons.map((icon, index) => (
-                <li key={index} className={s.icon} onClick={() => handleClick(index === 0 ? 'EcopointRegistration' : 'UserProfile')}>
-                    <FontAwesomeIcon icon={icon} />
-                </li>
-            ))}
-        </ul>
-    )
+    return (
+        <>
+            <Tooltip id="ecopoint-tooltip" />
+            <Tooltip id="profile-tooltip" />
+            <ul className={s.icons}>
+                {icons.map((icon, index) => (
+                    <li key={index} className={s.icon} onClick={() => handleClick(index === 0 ? 'EcopointRegistration' : 'UserProfile')}>
+                        <FontAwesomeIcon 
+                            icon={icon} 
+                            data-tooltip-id={index === 0 ? "ecopoint-tooltip" : "profile-tooltip"} 
+                            data-tooltip-content={index === 0 ? "Cadastre seu Ecoponto" : "Perfil"} />
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default IconsHeader;

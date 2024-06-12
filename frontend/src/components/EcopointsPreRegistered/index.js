0f2d4c3a3d750b2352342cb6ddbfdf4f39645b0b
Eduardo Marcon <emarconn@gmail.com>
2024-06-11 22:03:13 -0300

    feat: implementation modal for pre-registered ecopoints

--- frontend/src/components/EcopointsPreRegistered/index.js
+++ frontend/src/components/EcopointsPreRegistered/index.js
@@ -4,6 +4,8 @@ import Modal from 'react-modal';
 import style from './style.module.css';
 import { Slide, ToastContainer, toast } from 'react-toastify';
 import Loader from '../Loader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faEye } from '@fortawesome/free-solid-svg-icons';
 
 Modal.setAppElement('#root');
 
@@ -13,6 +15,7 @@ const EcopointsPreRegistered = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedEcopoint, setSelectedEcopoint] = useState(null);
     const [action, setAction] = useState('');
+    const [ecopointDetails, setEcopointDetails] = useState(null);
 
     const fetchEcopoints = async () => {
         setIsLoading(true);
@@ -26,6 +29,15 @@ const EcopointsPreRegistered = () => {
         }
     };
 
+    const fetchEcopointDetails = async (id) => {
+        try {
+            const response = await axios.get(`http://localhost:3001/ecopoints/${id}`);
+            setEcopointDetails(response.data);
+        } catch (err) {
+            console.error('Erro ao buscar detalhes do ecoponto', err);
+        }
+    };
+
     useEffect(() => {
         fetchEcopoints();
     }, []);
@@ -33,6 +45,9 @@ const EcopointsPreRegistered = () => {
     const handleOpenModal = (ecopoint, action) => {
         setSelectedEcopoint(ecopoint);
         setAction(action);
+        if (action === 'Visualizar') {
+            fetchEcopointDetails(ecopoint._id);
+        }
         setIsModalOpen(true);
     };
 
@@ -40,6 +55,7 @@ const EcopointsPreRegistered = () => {
         setIsModalOpen(false);
         setSelectedEcopoint(null);
         setAction('');
+        setEcopointDetails(null);
     };
 
     const handleConfirm = async () => {
@@ -47,8 +63,6 @@ const EcopointsPreRegistered = () => {
         setIsLoading(true);
         if (action === 'Aceitar') {
             await handleAccept(selectedEcopoint);
-        } else if (action === 'Rejeitar') {
-            await handleReject(selectedEcopoint._id);
         }
         setIsLoading(false);
         fetchEcopoints();
@@ -68,7 +82,6 @@ const EcopointsPreRegistered = () => {
 
             if (response.status === 200 || response.status === 201) {
                 await updateEcopoint(ecopoint);
-                
                 await sendEmailConfirmation(response.data.user);
 
                 toast.success('Ecoponto aceito.\nEmail enviado com sucesso!', {
@@ -116,11 +129,6 @@ const EcopointsPreRegistered = () => {
         }
     };
 
-    const handleReject = async (ecopointId) => {
-        alert('EM ANDAMENTO...');
-        /* CONSTRUÇÃO */
-    };
-
     return (
         <>
             <ToastContainer />
@@ -130,11 +138,21 @@ const EcopointsPreRegistered = () => {
                     <h1>Ecopontos não cadastrados</h1>
                     <ul>
                         {ecopoints.map((ecopoint) => (
-                            <li key={ecopoint._id}>
+                            <li key={ecopoint._id} className={style.listItem}>
                                 {ecopoint.companyName}
-                                <div>
-                                    <button onClick={() => handleOpenModal(ecopoint, 'Aceitar')}>Aceitar</button>
-                                    <button onClick={() => handleOpenModal(ecopoint, 'Rejeitar')}>Rejeitar</button>
+                                <div className={style.iconContainer}>
+                                    <FontAwesomeIcon
+                                        icon={faCheck}
+                                        className={style.iconCheck}
+                                        onClick={() => handleOpenModal(ecopoint, 'Aceitar')}
+                                        title='Aceitar'
+                                    />
+                                    <FontAwesomeIcon
+                                        icon={faEye}
+                                        className={style.iconEye}
+                                        onClick={() => handleOpenModal(ecopoint, 'Visualizar')}
+                                        title='Visualizar'
+                                    />
                                 </div>
                             </li>
                         ))}
@@ -147,10 +165,45 @@ const EcopointsPreRegistered = () => {
                     className={style.modal}
                     overlayClassName={style.modalOverlay}
                 >
-                    <h2>{action}?</h2>
-                    <p>Tem certeza que deseja {action} o ecoponto <strong>{selectedEcopoint?.companyName}</strong>?</p>
-                    <button className={style.btnConfirmModal} onClick={handleConfirm}>Confirmar</button>
-                    <button className={style.btnCloseModal} onClick={handleCloseModal}>Cancelar</button>
+                    {action === 'Aceitar' ? (
+                        <>
+                            <h2 className={style.modalTitle}>{action}?</h2>
+                            <p className={style.modalText}>Tem certeza que deseja {action} o ecoponto <strong>{selectedEcopoint?.companyName}</strong>?</p>
+                            <button className={style.btnConfirmModal} onClick={handleConfirm}>Confirmar</button>
+                            <button className={style.btnCloseModal} onClick={handleCloseModal}>Cancelar</button>
+                        </>
+                    ) : (
+                        <>
+                            <h2 className={style.modalTitle}>Detalhes</h2>
+                            {ecopointDetails ? (
+                                <div className={style.modalDetails}>
+                                    <p className={style.modalItem}><strong>Nome da Empresa:</strong> {ecopointDetails.companyName}</p>
+                                    <p className={style.modalItem}><strong>Email:</strong> {ecopointDetails.email}</p>
+                                    <p className={style.modalItem}><strong>Responsável:</strong> {ecopointDetails.responsibleName}</p>
+                                    <p className={style.modalItem}><strong>Telefone:</strong> {ecopointDetails.responsibleNumber}</p>
+                                    <p className={style.modalItem}><strong>Endereço:</strong> {ecopointDetails.companyStreet}, {ecopointDetails.companyNumber}, {ecopointDetails.companyComplement}, {ecopointDetails.companyDistrict}, {ecopointDetails.companyCity}, CEP: {ecopointDetails.companyCep}</p>
+                                    <p className={style.modalSubtitle}><strong>Resíduos aceitos:</strong></p>
+                                    <ul className={style.modalList}>
+                                        {ecopointDetails.residues.map(residue => (
+                                            <li key={residue._id} className={style.modalListItem}>{residue.name}</li>
+                                        ))}
+                                    </ul>
+                                    <p className={style.modalSubtitle}><strong>Horários de Funcionamento:</strong></p>
+                                    <ul className={style.modalList}>
+                                        {ecopointDetails.schedules.map(schedule => (
+                                            <li key={schedule._id} className={style.modalListItem}>
+                                                {['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'][schedule.weekDay]}: {schedule.startTime1} - {schedule.endTime1} {schedule.startTime2 && `, ${schedule.startTime2} - ${schedule.endTime2}`}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    <p className={style.modalItem}><strong>Aberto ao Público:</strong> {ecopointDetails.openToPublic ? 'Sim' : 'Não'}</p>
+                                </div>
+                            ) : (
+                                <p className={style.modalLoading}>Carregando...</p>
+                            )}
+                            <button className={style.btnCloseModal} onClick={handleCloseModal}>Fechar</button>
+                        </>
+                    )}
                 </Modal>
             </div>
         </>

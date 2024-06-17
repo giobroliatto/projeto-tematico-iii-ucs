4de66a0cd00ee35518a0b52c00f8bcb103b07704
Eduardo Marcon <emarconn@gmail.com>
2024-06-17 19:20:25 -0300

    style: changing icons for options in the admin profile

--- frontend/src/components/IconsHeader/index.js
+++ frontend/src/components/IconsHeader/index.js
@@ -1,44 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faFileCirclePlus, faFileCircleQuestion, faRecycle } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import s from './style.module.css';
 
 const initialIcons = [
     { icon: faFileCirclePlus, tooltipId: "ecopoint-tooltip", tooltipContent: "Cadastre seu Ecoponto", path: '/register-ecopoint' },
-    { icon: faRecycle, tooltipId: "recycle-tooltip", tooltipContent: "Resíduos", path: '/residues-form' },
     { icon: faUser, tooltipId: "profile-tooltip", tooltipContent: "Perfil", path: '/user-profile' }
 ];
 
 function IconsHeader() {
-    const [icons, setIcons] = useState(initialIcons);
-
-    useEffect(() => {
-        const checkAdminRole = () => {
-            const token = localStorage.getItem('token');
-            if (token !== "undefined" && token !== null) {
-                const base64Url = token.split('.')[1];
-                const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-                const decodedPayload = JSON.parse(atob(base64));
-                if (decodedPayload.role === 'admin') {
-                    setIcons(prevIcons => {
-                        if (prevIcons.length <= 3) {
-                            const newIcon = { 
-                                icon: faFileCircleQuestion, 
-                                tooltipId: "ecopoint-validation-tooltip", 
-                                tooltipContent: "Validar ecopontos", 
-                                path: '/ecopoints-pre-registered' 
-                            };
-                            return [...prevIcons.slice(0, 1), newIcon, ...prevIcons.slice(1)];
-                        }
-                        return prevIcons;
-                    });
-                }
-            }
-        };
-        checkAdminRole();
-    }, []);
+    const [icons ] = useState(initialIcons);
 
     return (
         <>
--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { MdAlternateEmail } from "react-icons/md";
 import axios from "axios";
@@ -12,6 +12,23 @@ const UserProfile = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const checkAdminRole = () => {
+            const token = localStorage.getItem('token');
+            if (token !== "undefined" && token !== null) {
+                const base64Url = token.split('.')[1];
+                const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+                const decodedPayload = JSON.parse(atob(base64));
+                if (decodedPayload.role === 'admin') {
+                    setIsAuthenticated(true);
+                }
+            }
+        };
+        checkAdminRole();
+    }, []);
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -20,19 +37,20 @@ const UserProfile = () => {
             const response = await axios.post('http://localhost:3001/auth/login', {
                 email,
                 password
-            })
+            });
 
-            if(response.status === 200 || response.status === 201) {
+            if (response.status === 200 || response.status === 201) {
                 const token = response.data.token;
-                localStorage.setItem('token', token);
 
-                navigate('/ecopoints-pre-registered');
-                
-            }   
+                /* GUARDAR O TOKEN NO LOCAL STORAGE */
+                localStorage.setItem('token', token); 
 
-        } catch(err){
+                setIsAuthenticated(true);
+            }
+
+        } catch (err) {
             const errorMessage = err.response.data.message;
-            
+
             toast.error(errorMessage, {
                 position: "top-right",
                 autoClose: 3000,
@@ -40,50 +58,64 @@ const UserProfile = () => {
                 draggable: true,
                 theme: 'dark',
                 transition: Slide
-            })
+            });
         }
-    }
+    };
 
     const handleBack = () => {
         navigate('/ecopoints');
-    }
+    };
 
     return (
         <>
             <ToastContainer />
-            <div className={styles.container}>
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <h1 className={styles.title1}>Acesso ao sistema</h1>
-                        <div className={styles.inputField}>
-                            <input 
-                                type="email"
-                                placeholder="Digite seu e-mail"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            <MdAlternateEmail className={`${styles.icon} ${styles.iconEmail}`}></MdAlternateEmail>
-                        </div>
-                        <div className={styles.inputField}>
-                            <input 
-                                type="password" 
-                                placeholder="Digite sua senha"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <RiLockPasswordFill className={`${styles.icon} ${styles.iconPassword}`}></RiLockPasswordFill>
-                        </div>
-
-                        <div className={styles.groupButtons}>
-                        
-                            <button className={styles.btn}>Entrar</button>
-
-                            <button onClick={handleBack} className={styles.btn}>Voltar</button>
-
-                        </div>
-                    </form>
+            {!isAuthenticated && (
+                <div className={styles.container}>
+                    <div>
+                        <form onSubmit={handleSubmit}>
+                            <h1 className={styles.title1}>Acesso ao sistema</h1>
+                            <div className={styles.inputField}>
+                                <input 
+                                    type="email"
+                                    placeholder="Digite seu e-mail"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <MdAlternateEmail className={`${styles.icon} ${styles.iconEmail}`} />
+                            </div>
+                            <div className={styles.inputField}>
+                                <input 
+                                    type="password" 
+                                    placeholder="Digite sua senha"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <RiLockPasswordFill className={`${styles.icon} ${styles.iconPassword}`} />
+                            </div>
+                            <div className={styles.groupButtons}>
+                                <button className={styles.btn} type="submit">Entrar</button>
+                                <button onClick={handleBack} className={styles.btn}>Voltar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            )}
+            {isAuthenticated && (
+                <div className={styles.containerAuth}>
+                    <h1 className={styles.titleAuth}>Acesso ao sistema</h1>
+                    <div className={styles.cardContainer}>
+                        <div className={styles.card} onClick={() => navigate('/register-ecopoint')}>
+                            <h2>Cadastrar Ecoponto</h2>
+                        </div>
+                        <div className={styles.card} onClick={() => navigate('/residues-form')}>
+                            <h2>Cadastrar Resíduo</h2>
+                        </div>
+                        <div className={styles.card} onClick={() => navigate('/ecopoints-pre-registered')}>
+                            <h2>Ecopontos Pré-Cadastrados</h2>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default UserProfile
+export default UserProfile;
